a99e9af07c45c3bdf648bdb3a05d0f787943a490
delaneydemark <ddemark@stuy.edu>
2018-02-05 18:55:19 -0500

    commented out factorial tests, fib throws exception if n<0

---
+++ Recursion.java
@@ -0,0 +1,27 @@
+public class Recursion{
+	
+	public static void main(String[] args){
+		//System.out.println(fact(0));
+		//System.out.println(fact(1));
+		//System.out.println(fact(2));
+		//System.out.println(fact(3));
+	}
+
+	public int fact(int n){
+		if (n<0){
+			throw new IllegalArgumentException();
+		}
+		if (n==0){
+			return 1;
+		}
+		else{
+			return n * fact(n-1);
+		}
+	}
+	
+	public static int fib(int n){
+		if (n<0){
+			throw new IllegalArgumentException();
+		}
+	}
+}
