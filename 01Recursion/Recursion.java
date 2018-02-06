a4051e860b830a7d6d4dbc5a8068bb4ea9eef903
delaneydemark <ddemark@stuy.edu>
2018-02-05 19:40:32 -0500

    fib calls the helper function, the helper function uses tail recursion

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -5,6 +5,15 @@ public class Recursion{
 		//System.out.println(fact(1));
 		//System.out.println(fact(2));
 		//System.out.println(fact(3));
+		
+		// 0
+		System.out.println(fib(0));
+		// 1
+		System.out.println(fib(1));
+		// 1
+		System.out.println(fib(2));
+		// 2
+		System.out.println(fib(3));
 	}
 
 	public int fact(int n){
@@ -23,5 +32,16 @@ public class Recursion{
 		if (n<0){
 			throw new IllegalArgumentException();
 		}
+		return fibHelper(0, 1, n);
+	}
+	
+	public static int fibHelper(int other, int sum, int n){
+		if (n==0){
+			return 0;
+		}
+		if (n==1){
+			return 1;
+		}
+		return fibHelper(sum, other + sum, n-1);
 	}
 }
