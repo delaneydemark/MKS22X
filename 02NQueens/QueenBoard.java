64f601c6f9d23355baced64226cc453161f78f8a
delaneydemark <ddemark@stuy.edu>
2018-02-09 14:18:36 -0500

    wrote toString

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -2,9 +2,20 @@ public class QueenBoard{
     private int[][] board;
 
     public QueenBoard(int size){
-	int[][] board = new int[size][size];
+	board = new int[size][size];
     }
 
-
-
+    public String toString(){
+	String s = "";
+	for(int i = 0; i<board.length; i++){
+	    for(int j = 0; j<board[0].length; j++){
+		if(board[i][j]==-1){
+		    s+="Q";
+		}else{
+		    s+="_";
+		}
+	    }
+	}
+	return s;
+    }
 }
