df27dc7f16025c2b8c7719b43c42ff462f55df6b
delaneydemark <ddemark@stuy.edu>
2018-02-11 16:26:05 -0500

    if the board is solvable, then countSolutions sums the queens

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -142,15 +142,17 @@ public class QueenBoard{
 		}
 	    }
 	}
-	solve();
-	int sum = 0;
-	for (int i = 0; i<board.length; i++){
-	    for (int j = 0; j<board[i].length; j++){
-		if (board[i][j]==-1){
-		    sum+=1;
+	if(this.solve()){
+	    int sum = 0;
+	    for (int i = 0; i<board.length; i++){
+		for (int j = 0; j<board[i].length; j++){
+		    if (board[i][j]==-1){
+			sum+=1;
+		    }
 		}
 	    }
 	}
+	
 	return sum;
     }
 
