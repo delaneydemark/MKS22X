5529a114e3024075d38704541f47bf74b2a6c98f
delaneydemark <ddemark@stuy.edu>
2018-02-25 16:18:10 -0500

    changed countSolutions to return an int again. within the loop, it checks that the next move is possible & that the position is 0, changes the position to the level, adds countSolutionsH of the next move to count, removes knight

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -2,11 +2,9 @@ public class KnightBoard{
     int board[][];
     int possibleMoves[][] = {{2,1},{1,2},{-1,-2},{-2,-1},
 			     {-1,2},{2,-1},{-2,1},{1,-2}};
-    int countSol;
 
     public KnightBoard(int startingRows, int startingCols){
 	board = new int[startingRows][startingCols];
-	countSol=0;
     }
 
     public String toString(){
@@ -90,31 +88,31 @@ public class KnightBoard{
 	if(startingRow<0 || startingRow>=board.length || startingCol<0 || startingCol>=board[0].length){
 	    throw new IllegalArgumentException();
 	}
-	countSolutionsH(startingRow, startingCol, 1);
-	return countSol;
+	return countSolutionsH(startingRow, startingCol, 1);
     }
 
-    public boolean countSolutionsH(int row, int col, int level){
+    public int countSolutionsH(int row, int col, int level){
+	int count = 0;
 	if(level> board.length*board[0].length){
-	    countSol+=1;
+	    return 1;
 	}
-	if(row<0 || row>=board.length || col<0 || col>=board[0].length){
-	    return false;
-	}
-	if(board[row][col]!=0){
-	    return false;
-	}
-	board[row][col]=level;
 	for(int i = 0; i<possibleMoves.length; i++){
-	    return countSolutionsH(row+possibleMoves[i][0], col+possibleMoves[i][1], level+1);
+	    if(row+possibleMoves[i][0]>=0 && row+possibleMoves[i][0]<board.length &&
+	       col+possibleMoves[i][1]>=0 && col+possibleMoves[i][1]<board[0].length){
+		if(board[row][col]==0){
+		    board[row][col]=level;
+		    count+=countSolutionsH(row+possibleMoves[i][0], col+possibleMoves[i][1], level+1);
+		    board[row][col]=0;
+		}
+	    }
+	    
 	}
-	board[row][col]=0;
-	return true;
+	return count;
     }
 
     public static void main(String[] args){
-	KnightBoard test = new KnightBoard(7,7);
-	System.out.println(test.toString());
+	KnightBoard test = new KnightBoard(5,5);
+	//System.out.println(test.toString());
 	//System.out.println(test.solve(2,2));
 	//System.out.println(test.toString());
 	System.out.println(test.countSolutions(2,2));
