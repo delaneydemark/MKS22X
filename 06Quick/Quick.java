746732d5ae5ac63028824a7e1575145a2589796d
delaneydemark <ddemark@stuy.edu>
2018-03-13 21:38:47 -0400

    for each element in data, if the index returned by partition is the index of k, the element is returned. otherwise, the start or the end is adjusted. written but not tested

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -24,15 +24,34 @@ public class Sorts{
 		data[start] = sw;
 		return end;
     }
+
+    public static int quickselect(int[] data, int k){
+	int index = 0;
+	int start = 0;
+	int end = data.length-1;
+	for(int i = 0; i<data.length; i++){
+	    index = partition(data, start, end);
+	    if(index==k){
+		return data[k];
+	    }else if(k<=index){
+		end = index-1;
+	    }else if(k>index){
+		start = index+1;
+	    }
+	}
+	return data[k];
+    }
     public static void main(String[] args){
 		int[] arr = {999,999,999,4,1,0,3,2,999,999,999};
-		int index = Sorts.partition(arr, 0, arr.length-1);
+		/*int index = Sorts.partition(arr, 0, arr.length-1);
 		System.out.println(index);
 		String s = "";
 		for(int i = 0; i<arr.length; i++){
 			s+=" " + arr[i];
 		}
-		System.out.println(s);
+		System.out.println(s);*/
+		
+
 		
     }
 }
