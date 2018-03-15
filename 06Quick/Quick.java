d456674157533ac7908828499d4922ba4a56c7d3
delaneydemark <ddemark@stuy.edu>
2018-03-14 20:04:16 -0400

    fixed partition

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,28 +1,30 @@
 import java.util.Random;
 
-public class Sorts{
+public class Quick{
     
     public static int partition(int[] data, int start, int end){
-		Random rand = new Random();
-		int pivot = rand.nextInt(data.length);
-		int pVal = data[pivot];
-		data[pivot] = data[start];
-		data[start] = pVal;
-		int small = start + 1;
-		while(small<=end ){
-			if(data[small]>pVal){
-	    		int swap = data[end];
-	    		data[end] = data[small];
-	    		data[small] = swap;
-	    		end--;
-	    	}else if(data[small]<=pVal){
-	    		small++;
-	    	}
-		}
-		int sw = data[end];
-		data[end] = pVal;
-		data[start] = sw;
-		return end;
+	Random rand = new Random();
+	int pivot = rand.nextInt(end - start) + start;
+
+	int pVal = data[pivot];
+	data[pivot] = data[start];
+	data[start] = pVal;
+	int small = start + 1;
+	int large = end;
+	while(small<=large ){
+	    if(data[small]>=pVal){
+		int swap = data[large];
+		data[large] = data[small];
+		data[small] = swap;
+		large--;
+	    }else if(data[small]<pVal){
+		small++;
+	    }
+	}
+	int sw = data[large];
+	data[large] = pVal;
+	data[start] = sw;
+	return large;
     }
 
     public static int quickselect(int[] data, int k){
@@ -43,13 +45,20 @@ public class Sorts{
     }
     public static void main(String[] args){
 		int[] arr = {999,999,999,4,1,0,3,2,999,999,999};
-		/*int index = Sorts.partition(arr, 0, arr.length-1);
+		/*int index = Quick.partition(arr, 0, 7);
 		System.out.println(index);
 		String s = "";
 		for(int i = 0; i<arr.length; i++){
 			s+=" " + arr[i];
 		}
 		System.out.println(s);*/
+		int k = Quick.quickselect(arr, 3);
+		System.out.println(k);
+		String s = "";
+		for(int i = 0; i<arr.length; i++){
+			s+=" " + arr[i];
+		}
+		System.out.println(s);
 		
 
 		
