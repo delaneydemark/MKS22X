c7337470ba5c01b18575731e92aaaf1f0848815f
delaneydemark <ddemark@stuy.edu>
2018-03-27 21:04:32 -0400

    wrote gets and sets for Node

---
+++ 08LinkedLists/MyLinkedList.java
@@ -0,0 +1,34 @@
+public class MyLinkedList{
+    Node first;
+    Node last;
+    int length;
+
+    private class Node{
+	Node next, prev;
+	int data;
+
+	public Node getNext(){
+	    return next;
+	}
+
+	public Node getPrev(){
+	    return prev;
+	}
+
+	public int getValue(){
+	    return data;
+	}
+
+	public void setNext(Node n){
+	    next = n;
+	}
+
+	public void setPrev(Node p){
+	    prev = p;
+	}
+
+	public void setValue(int i){
+	    data = i;
+	}
+    }
+}
