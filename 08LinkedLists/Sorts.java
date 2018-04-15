d30cd1d0f984f4a0e5a355f4e6d1c87be29e142d
delaneydemark <ddemark@stuy.edu>
2018-04-15 13:42:15 -0400

    fixed typos, added import

--- 08LinkedLists/Sorts.java
+++ 08LinkedLists/Sorts.java
@@ -1,8 +1,9 @@
 import java.util.*;
+import java.io.*;
 
 public class Sorts{
     public static void radixsort(MyLinkedListImproved<Integer> data){
-	@SupressWarnings("unchecked") MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved[10];
+	@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved[10];
 
 	int maxI = data.max();
 	Integer max = data.get(maxI);
@@ -12,13 +13,24 @@ public class Sorts{
     public static int digits(Integer n){
 	int sum = 0;
 	if(n<0){
-	    num *= -1;
+	    n *= -1;
 	}
 	while(n>=1){
 	    sum++;
 	    n/=10;
 	}
 	return sum;
+    }
+
+    public static void main(String[] args){
+	MyLinkedListImproved<Integer> test = new MyLinkedListImproved<Integer>();
+	test.add(3);
+	test.add(2);
+	test.add(57);
+	test.add(9);
+	test.add(666);
+	System.out.println(test.toString());
+	
     }
 
     
