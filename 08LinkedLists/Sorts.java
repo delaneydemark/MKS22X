a8be39217ac6d8b4d5e7ff195220f92cf4cda0f1
delaneydemark <ddemark@stuy.edu>
2018-04-15 13:12:51 -0400

    created array of linked lists for buckets

--- 08LinkedLists/Sorts.java
+++ 08LinkedLists/Sorts.java
@@ -1,4 +1,7 @@
+import java.util.*;
+
 public class Sorts{
 	public static void radixsort(MyLinkedListImproved<Integer> data){
+	    @SupressWarnings("unchecked") MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved[10];
 	}
 }
