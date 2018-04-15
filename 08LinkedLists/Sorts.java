92dcf6a002ea30ce958457f2e1e7e71556a17460
delaneydemark <ddemark@stuy.edu>
2018-04-15 13:22:27 -0400

    radix sort gets the max of the list and will find the number of digits in the max; shell created for digits

--- 08LinkedLists/Sorts.java
+++ 08LinkedLists/Sorts.java
@@ -1,7 +1,15 @@
 import java.util.*;
 
 public class Sorts{
-	public static void radixsort(MyLinkedListImproved<Integer> data){
-	    @SupressWarnings("unchecked") MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved[10];
-	}
+    public static void radixsort(MyLinkedListImproved<Integer> data){
+	@SupressWarnings("unchecked") MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved[10];
+
+	int maxI = data.max();
+	Integer max = data.get(maxI);
+    }
+
+    private static int digits(Integer n){
+    }
+
+    
 }
