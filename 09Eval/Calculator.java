ae83962c9fe474856191d6a201e3fb0931e637cb
delaneydemark <ddemark@stuy.edu>
2018-04-18 15:11:59 -0400

    fixed filename

---
+++ 09Eval/Calculator.java
@@ -0,0 +1,31 @@
+public class Eval{
+    public static double eval(String nums){
+	Stacks expression = new Stacks();
+	String[] tokens = nums.split(" ");
+	for(int i = 0; i<tokens.length; i++){
+	    if(tokens[i].equals("+")){
+		expression.push(expression.pop() + expression.pop());
+	    }else if(tokens[i].equals("-")){
+		expression.push(-1*(expression.pop() - expression.pop()));
+	    }else if(tokens[i].equals("*")){
+		expression.push(expression.pop() * expression.pop());
+	    }else if(tokens[i].equals("/")){
+		expression.push((1/expression.pop()) * expression.pop());
+	    }else if(tokens[i].equals("%")){
+		double current = expression.pop();
+		expression.push(expression.pop() % current);
+	    }else{
+		double num = Double.parseDouble(tokens[i]);
+		expression.push(num);
+	    }
+	}
+	return expression.peek();
+    }
+
+    public static void main(String[] args){
+	System.out.println(Calculator.eval("10 2.0 +"));
+	System.out.println(Calculator.eval("11 3 - 4 + 2.5 *"));
+	System.out.println(Calculator.eval("8 2 + 99 9 - * 2 + 9 -"));
+    }
+   
+}
