47e9cc1ede0ae607bdac4143cd93d4e691c91482
delaneydemark <ddemark@stuy.edu>
2018-04-17 21:42:59 -0400

    wrote stacks constructor and push

--- 09Eval/Stacks.java
+++ 09Eval/Stacks.java
@@ -1,4 +1,13 @@
 import java.util.*;
 
 public class Stacks{
+    private ArrayList<Double> data;
+
+    public Stacks(){
+	data = new ArrayList<Double>();
+    }
+
+    public void push(Double n){
+	data.add(d);
+    }
 }
