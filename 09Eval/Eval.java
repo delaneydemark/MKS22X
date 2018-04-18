976696cd8127daeb12b14b50e678aa5943e0d7ec
delaneydemark <ddemark@stuy.edu>
2018-04-17 22:09:05 -0400

    working through if statements for each operation

--- 09Eval/Eval.java
+++ 09Eval/Eval.java
@@ -2,5 +2,12 @@ public class Eval{
     public static double eval(String nums){
 	Stacks expression = new Stacks();
 	String[] tokens = nums.split(" ");
+	for(int i = 0; i<tokens.length; i++){
+	    if(tokens[i].equals("+")){
+		expression.push(expression.pop() + expression.pop());
+	    }else if(tokens[i].equals("-")){
+		expression.push(-1*(expression.pop() - expression.pop()));
+	    }
+	}
     }
 }
