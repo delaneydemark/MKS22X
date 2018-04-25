822aaa1050d91ee17e0593b6c6ad5b9fc475a9e5
delaneydemark <ddemark@stuy.edu>
2018-04-24 21:06:40 -0400

    all tests work

--- 11Tree/ExpressionTree.java
+++ 11Tree/ExpressionTree.java
@@ -63,14 +63,14 @@ public class ExpressionTree{
 	if(isValue()){
 	    return "" + getValue() + " ";
 	}
-	return "" + getOp() + " " + getLeft().toStringPrefix() + getRight.toStringPrefix();
+	return "" + getOp() + " " + getLeft().toStringPrefix() + getRight().toStringPrefix();
     }
 
     public double evaluate(){
 	if(isValue()){
 	    return getValue();
 	}
-	return apply(getOp(), getLeft().evaluate(), getRight.evaluate());
+	return apply(getOp(), getLeft().evaluate(), getRight().evaluate());
     }
 
     private double apply(char op, double a, double b){
@@ -83,5 +83,43 @@ public class ExpressionTree{
 	}else if(op=='/'){
 	    return a/b;
 	}
+	return 0.0;
+    }
+
+    public static void main(String[] args){
+	//ugly main sorry!
+	ExpressionTree a = new ExpressionTree(4.0);
+	ExpressionTree b = new ExpressionTree(2.0);
+
+	ExpressionTree c = new ExpressionTree('+',a,b);
+	System.out.println(c);
+	System.out.println(c.toStringPostfix());
+	System.out.println(c.toStringPrefix());
+	System.out.println(c.evaluate());//6.0
+
+	ExpressionTree d = new ExpressionTree('*',c,new ExpressionTree(3.5));
+	System.out.println(d);
+	System.out.println(d.toStringPostfix());
+	System.out.println(d.toStringPrefix());
+	System.out.println(d.evaluate());//21
+	
+	ExpressionTree ex = new ExpressionTree('-',d,new ExpressionTree(1.0));
+	System.out.println(ex);
+	System.out.println(ex.toStringPostfix());
+	System.out.println(ex.toStringPrefix());
+	System.out.println(ex.evaluate());//20
+	
+	ex = new ExpressionTree('+',new ExpressionTree(1.0),ex);
+	System.out.println(ex);
+	System.out.println(ex.toStringPostfix());
+	System.out.println(ex.toStringPrefix());
+	System.out.println(ex.evaluate());//21
+	
+	ex = new ExpressionTree('/',ex,new ExpressionTree(2.0));
+	System.out.println(ex);
+	System.out.println(ex.toStringPostfix());
+	System.out.println(ex.toStringPrefix());
+	System.out.println(ex.evaluate());//10.5   
+	
     }
 }
