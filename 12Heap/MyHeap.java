4569fe54024bede1bef508fe901063d5ea6be059
delaneydemark <ddemark@stuy.edu>
2018-04-26 21:29:11 -0400

    started heap

---
+++ 12Heap/MyHeap.java
@@ -0,0 +1,33 @@
+public class MyHeap{
+
+    private String[] data;
+    private int size;
+    
+    
+
+    public MyHeap(){
+	data = new String[10];
+	size = 0;
+    }
+
+    public MyHeap(boolean b){
+	
+    }
+
+    public void add(String s){
+	
+    }
+
+    public String remove(){
+	
+    }
+
+    public String peek(){
+	
+    }
+
+    public int size(){
+	
+    }
+	
+}
