b25eeecfd7262b4671928ec8404443ee1ebb0890
delaneydemark <ddemark@stuy.edu>
2018-04-26 21:31:16 -0400

    t is true when its a max heap, t is false when its a min heap

--- 12Heap/MyHeap.java
+++ 12Heap/MyHeap.java
@@ -2,16 +2,20 @@ public class MyHeap{
 
     private String[] data;
     private int size;
+    private boolean t;
     
     
 
     public MyHeap(){
 	data = new String[10];
 	size = 0;
+	t = true;
     }
 
     public MyHeap(boolean b){
-	
+	data = new String[10];
+	size = 0;
+	t = b;
     }
 
     public void add(String s){
