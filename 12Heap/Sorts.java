66764f28a4ac8c051498212433767f13e3676653
delaneydemark <ddemark@stuy.edu>
2018-04-30 21:50:18 -0400

    wrote heapsort & tested

---
+++ 12Heap/Sorts.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Sorts{
+    public static void heapsort(Integer[] arr){
+	MyHeap<Integer> nums = MyHeap.heapify(arr);
+	System.out.println(Arrays.toString(arr));
+	for(int i = 0; i<nums.size(); i++){
+	    Integer val = nums.remove();
+	    arr[nums.size()] = val;
+	}
+    }
+
+    public static void main(String[] args){
+	Integer[] test = new Integer[5];
+	test[0] = 4;
+	test[1] = 7;
+	test[2] = 10;
+	test[3] = 5;
+	test[4] = 3;
+	heapsort(test);
+	System.out.println(Arrays.toString(test));
+    }
+}
