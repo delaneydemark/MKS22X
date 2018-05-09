2ebbc67655a6bdf9ae94a143682bb11c1b611a53
delaneydemark <ddemark@stuy.edu>
2018-05-09 14:52:30 -0400

    Location implements comparable and wrote constructor w distance

--- 14FrontierMazeSolver/Location.java
+++ 14FrontierMazeSolver/Location.java
@@ -1,4 +1,4 @@
-public class Location{
+public class Location implements Comparable<Location>{
     private int x,y;
     private Location previous;
     private int distanceToStart;
@@ -7,6 +7,13 @@ public class Location{
 	x = _x;
 	y = _y;
 	previous = prev;
+    }
+
+    public Location(int _x, int _y, Location prev, int d){
+	x = _x;
+	y = _y;
+	previous = prev;
+	distanceToStart = d;
     }
 
     public int getX(){
