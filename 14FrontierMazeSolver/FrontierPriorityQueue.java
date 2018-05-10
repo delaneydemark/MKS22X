bfa0a67cd1435231ffe1732662c81559de0652f3
delaneydemark <ddemark@stuy.edu>
2018-05-10 14:17:17 -0400

    wrote priority queue

--- 14FrontierMazeSolver/FrontierPriorityQueue.java
+++ 14FrontierMazeSolver/FrontierPriorityQueue.java
@@ -1,2 +1,20 @@
 public class FrontierPriorityQueue{
+    MyHeap<Location> locations;
+
+    public FrontierPriorityQueue(){
+	locations = new MyHeap<Location>(false);
+    }
+
+    public void add(Location n){
+	locations.add(n);
+    }
+
+    public boolean hasNext(){
+	return !locations.size()==0;
+    }
+
+    public Location next(){
+	return locations.remove();
+    }
+		    
 }
